a3b07e0dd9a2d0b9f77fea536317207469b60339
RoboMWM <robomwm@users.noreply.github.com>
2016-05-06 00:46:15 -0700

    updating

--- src/main/java/net/poweredbyhate/wildtp/WildTP.java
+++ src/main/java/net/poweredbyhate/wildtp/WildTP.java
@@ -1,6 +1,10 @@
 package net.poweredbyhate.wildtp;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -17,4 +21,31 @@ public class WildTP extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new SignChangeListener(), this);
         Bukkit.getPluginManager().registerEvents(new SignClickListener(), this);
     }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String args[])
+    {
+        boolean tacos = true;
+        if (cmd.getName().equalsIgnoreCase("Wild"))
+        {
+            if (!(sender instanceof Player))
+            {
+                sender.sendMessage("Oh hello there server master, would you like me to randomly teleport your serbur somewhere else?");
+                return tacos;
+            }
+
+            final Player player1 = (Player)sender;
+            final Player player = (Player)sender;
+
+            if (!player1.hasPermission("Wild.wildtp"))
+            {
+                Player play_er = (Player)sender;
+                sender.sendMessage(ChatColor.RED + "Hey u dont have permsioson to /wild! D: pls ask admin why");
+                return tacos;
+            }
+            new TeleportGoneWild().WildTeleport(player);
+
+        }
+        return tacos;
+    }
+
 }
