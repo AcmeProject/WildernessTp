5f1e46723d3fdc6a2c7bcaf0bdda3a0e7b4fd482
RoboMWM <robomwm@users.noreply.github.com>
2016-09-21 08:35:23 -0700

    First of many more rewrites
    yea write

--- src/main/java/net/poweredbyhate/wildtp/CommandsGoneWild.java
+++ src/main/java/net/poweredbyhate/wildtp/CommandsGoneWild.java
@@ -22,7 +22,11 @@ public class CommandsGoneWild implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         WildTP.debug("Wild command called by " + sender);
 
-        if (sender.hasPermission("wild.wildtp") && args.length == 0) {
+        if (args.length >= 1 && sender.getServer().getPlayer(args[0]) != null && sender.hasPermission("wild.wildtp.others")) {
+            WildTP.debug(sender.getName() + " Called /wild args " + args[0]);
+            new TeleportGoneWild().WildTeleport(sender.getServer().getPlayer(args[0]));
+        }
+        else if (sender.hasPermission("wild.wildtp")) {
             if (!(sender instanceof Player)) {
                 Bukkit.getConsoleSender().sendMessage(ChatColor.MAGIC + "One does not simply go wild.");
                 return false;
@@ -31,12 +35,8 @@ public class CommandsGoneWild implements CommandExecutor {
             WildTP.debug(p.getName() + " called /wild args 0");
             new TeleportGoneWild().WildTeleport(p);
         }
-        if (args.length >= 1) {
-            if (sender.getServer().getPlayer(args[0]) != null && sender.hasPermission("wild.wildtp.others")) {
-                WildTP.debug(sender.getName() + " Called /wild args " + args[0]);
-                new TeleportGoneWild().WildTeleport(sender.getServer().getPlayer(args[0]));
-            }
-        }
+        else
+            sender.sendMessage(TooWildForEnums.translate(TooWildForEnums.NO_PERMS));
         return false;
     }
 }
