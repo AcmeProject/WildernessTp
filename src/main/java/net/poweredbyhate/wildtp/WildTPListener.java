800d8747188dbb4ac149af4d4cce5316bb7fb7aa
Simon <67807644+KoxSosen@users.noreply.github.com>
2021-04-17 21:03:22 +0200

    Volume

--- src/main/java/net/poweredbyhate/wildtp/WildTPListener.java
+++ src/main/java/net/poweredbyhate/wildtp/WildTPListener.java
@@ -64,7 +64,7 @@ public class WildTPListener implements Listener {
 
         try
         {
-            String volume = WildTP.instace.getConfig().getString("Volume-Radius");
+            String volume = WildTP.instace.getConfig().getString("Volume");
             plaza.playSound(lok, Sound.valueOf(sound), SoundCategory.MASTER, Float.parseFloat(String.valueOf(volume)), 1);
             WildTP.debug("Playing da sound of " + sound + " with volume or radius of  " + volume);
         }
